3b3681b3a0862021d402c3841cdbdd560b774ab7
Rafael Blum <rafaelblum_digital@hotmail.com>
2024-05-15 22:31:40 -0300

    :book: feat: add tailwindcss and scroolreveal

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,28 @@
 import './bootstrap';
+import ScrollReveal from "scrollreveal";
+import JSConfetti from 'js-confetti';
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 4000,
+    delay: 400,
+});
+
+sr.reveal('.test', {origin: 'top', distance: '30px', duration: 4000, delay: 400,});
+sr.reveal('.logo', {origin: 'top', duration: 1500});
+sr.reveal('.button', {origin: 'right'});
+sr.reveal('.description', {origin: 'bottom'});
+sr.reveal('.footer', {origin: 'bottom'});
+
+
+
+
+const jsConfetti = new JSConfetti();
+
+window.confetti = ()=> jsConfetti.addConfetti(
+    {
+        emojis: ['⭐'],
+    }
+);
+
