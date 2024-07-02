651ab7d5f059031cdeba0d07a1bf68a84de32744
Rafael Blum <rafaelblum_digital@hotmail.com>
2024-07-02 13:12:06 -0300

    refactor: ajuste about

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -28,12 +28,21 @@
             <div class="flex flex-col items-center text-gray-900 dark:text-white">
                 <x-partials.header-section
                     title="Sobre"
-                    description="Quem somos"/>
+                    description="Sejá bem-vindo a comunidade"/>
 
                     {{--  --}}
                 <p class="mb-8 font-light lg:text-xl">
-                    Deliver great service experiences fast - without the complexity of traditional ITSM solutions.
-                    Accelerate critical development work, eliminate toil, and deploy changes with ease.</p>
+                    Bem-vindo à Retrô Community, sua comunidade dedicada aos clássicos dos games retrô! Explore nosso blog repleto de artigos atualizados sobre gameplays, eventos e análises detalhadas.
+                </p>
+                <p class="mb-8 font-light lg:text-xl">
+                    Na Retrô Community, celebramos não apenas os jogos que marcaram época, mas também fortalecemos a comunidade através do apoio a nossos parceiros do YouTube. Trabalhamos com criadores apaixonados que compartilham histórias, dicas valiosas e momentos inesquecíveis em vídeos envolventes. Aqui, o bate-papo é tão importante quanto a jogatina, proporcionando uma plataforma onde os fãs de games retrô podem se reunir para discutir seus títulos favoritos e descobrir novos clássicos.
+                </p>
+                <p class="mb-8 font-light lg:text-xl">
+                    Nosso blog oferece atualizações regulares sobre eventos de gaming, análises de gameplays icônicos e previews de novos lançamentos imperdíveis. Além disso, exploramos a história dos videogames e as tendências atuais da indústria em artigos informativos e cativantes.
+                </p>
+                <p class="mb-8 font-light lg:text-xl">
+                    Prepare-se para uma viagem no tempo através dos mundos pixelizados que capturaram nossa imaginação, enquanto celebramos a magia duradoura dos games retrô. Junte-se à Retrô Community, onde o passado e o presente se encontram em uma comunidade apaixonada por videogames clássicos.
+                </p>
             </div>
         </div>
     </section>
@@ -47,6 +56,29 @@
                     Free trial for 30 days
                 </a>
             </div>
+        </div>
+    </section>
+
+    <section class="relative bg-gray-50 dark:bg-gray-800 isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div class="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20"></div>
+        <div class="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-gray-50 dark:bg-gray-800 shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center"></div>
+        <div class="mx-auto max-w-2xl lg:max-w-4xl">
+            <img class="mx-auto h-12" src="/images/brandname/horizontal-retrocommunity.png" alt="">
+            <figure class="mt-10">
+                <blockquote class="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
+                    <p>“Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias molestiae. Numquam corrupti in laborum sed rerum et corporis.”</p>
+                </blockquote>
+                <figcaption class="mt-10">
+                    <img class="mx-auto h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                    <div class="mt-4 flex items-center justify-center space-x-3 text-base">
+                        <div class="font-semibold text-gray-900">Judith Black</div>
+                        <svg viewBox="0 0 2 2" width="3" height="3" aria-hidden="true" class="fill-gray-900">
+                            <circle cx="1" cy="1" r="1" />
+                        </svg>
+                        <div class="text-gray-600">CEO of Workcation</div>
+                    </div>
+                </figcaption>
+            </figure>
         </div>
     </section>
 
