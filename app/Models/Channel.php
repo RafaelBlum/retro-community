273b2063d70794e38331145933ad1bc0ede7057b
Rafael Blum <rafaelblum_digital@hotmail.com>
2024-07-08 22:01:02 -0300

    feat: add migrate e properti campaing

--- app/Models/Channel.php
+++ app/Models/Channel.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Channel extends Model
 {
@@ -27,5 +28,10 @@ class Channel extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function channel(): HasOne
+    {
+        return $this->hasOne(Campaing::class);
     }
 }
---
+++ database/migrations/2024_07_08_214307_add_properties_to_campaings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('campaings', function (Blueprint $table) {
+            $table->foreignId('channel_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('title');
+            $table->text('content');
+            $table->string('linkGoal');
+            $table->string('qrCode')->nullable();
+            $table->boolean('camping');
+            $table->string('image')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('campaings', function (Blueprint $table) {
+            //
+        });
+    }
+};
