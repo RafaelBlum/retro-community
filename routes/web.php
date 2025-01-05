a749e21fcfa6ac68f866df5bc69214b526f8762a
Rafael Blum <rafaelblum_digital@hotmail.com>
2025-01-05 10:24:37 -0300

    add route channels

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [WebController::class, 'landing'])->name('app.landing');
 Route::get('home', [WebController::class, 'home'])->name('app.home');
 Route::get('/sobre', [WebController::class, 'about'])->name('app.about');
+Route::get('/Canais', [ChannelController::class, 'channels'])->name('app.channels');
 Route::get('/politicas', [WebController::class, 'policy'])->name('app.policy');
 Route::get('/suporte', [WebController::class, 'support'])->name('app.support');
 Route::get('/campanhas', [CampaingController::class, 'index'])->name('app.campaings');
