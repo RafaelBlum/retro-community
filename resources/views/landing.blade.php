2d5306da33d1c35d4c231e21f8dad42c6e8bf011
Rafael Blum <rafaelblum_digital@hotmail.com>
2025-06-18 09:53:57 -0300

    updadtes

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -46,7 +46,7 @@
                     </ul>
                 </div>
 
-                <footer class="py-16 text-center text-sm text-black dark:text-white/70">
+                <footer class="py-16 text-center text-sm text-black dark:text-white/95">
                     Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                 </footer>
             </section>
