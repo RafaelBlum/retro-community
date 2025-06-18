68c7530deaa2c97148fcdef7db1956cf4fd49b3c
Rafael Blum <rafaelblum_digital@hotmail.com>
2025-06-18 10:24:35 -0300

    updated Tailwind css v.4

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from "tailwindcss";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -8,14 +8,6 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        tailwindcss(),
     ],
-    css: {
-        postcss: {
-            plugins: [
-                tailwindcss({
-                    config: "./tailwind.config.js",
-                }),
-            ],
-        },
-    },
 });
